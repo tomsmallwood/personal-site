dc3f3e042539ae6337ef4d726f8767e711936970
Thomas Smallwood <thomasinio@hotmail.com>
2024-03-02 00:42:28 +0000

    increase rrset ttl

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "sub_ns" {
   zone_id = data.aws_route53_zone.top.zone_id
   name    = var.subdomain_name
   type    = "NS"
-  ttl     = 1
+  ttl     = 172800
   records = aws_route53_zone.sub.name_servers
 }
 
@@ -21,7 +21,7 @@ resource "aws_route53_record" "pages_v6" {
   zone_id = aws_route53_zone.sub.id
   name    = var.subdomain_name
   type    = "AAAA"
-  ttl     = 1
+  ttl     = 30
   records = var.pages_ips_v6
 }
 
@@ -30,6 +30,6 @@ resource "aws_route53_record" "pages_v4" {
   zone_id = aws_route53_zone.sub.id
   name    = var.subdomain_name
   type    = "A"
-  ttl     = 1
+  ttl     = 30
   records = var.pages_ips_v4
 }
